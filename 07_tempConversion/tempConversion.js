312fdba1b22a20b153a8aa09bef49e5f0d192b62
Sean Marshall <smarshallean@gmail.com>
2022-03-16 23:18:39 +1000

    Completed activity 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(num) {
+  let result = (num - 32) * 0.5556;
+  return Math.round(result * 10)/10;
 };
 
-const ctof = function() {
-
+const ctof = function(num) {
+  let result =  (num * 1.8) + 32;
+  return Math.round(result * 10)/10;
 };
 
 // Do not edit below this line
@@ -11,3 +13,5 @@ module.exports = {
   ftoc,
   ctof
 };
+
+// 1.744 --> 1.7
