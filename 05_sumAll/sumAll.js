6e7adc7ebeb20da84d3f5bc88f0230566f806922
Sean Marshall <smarshallean@gmail.com>
2022-03-16 22:55:35 +1000

    Completed exercises 5 and 6

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let result = start + end;
+    if ((typeof start !== 'number' || typeof end !== 'number' ) 
+    || (start < 0 || end < 0)) {
+        return 'ERROR';
+    }
+    if (start > end) {
+        oldStart = start;
+        start = end;
+        end = oldStart;
+    }  
+    for (let numToAdd = start + 1; numToAdd < end; numToAdd++) {
+         result += numToAdd;
+    }
+    return result;
 };
+
+sumAll(4,1)
+// [1,4] -> [1,2,3,4]
 
 // Do not edit below this line
 module.exports = sumAll;
